45c3bb6d69eeba64be37ab56413ae7b05704b08b
seby19 <sebyxavier19@gmail.com>
2019-01-27 03:29:40 +0530

    portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Portfolio';
+export class AppComponent implements OnInit{
+  menuNum = 0;
+  ngOnInit() {
+    this.menuNum = 0;
+  }
+  setMenuNum(number) {
+    this.menuNum = number;
+  }
 }
