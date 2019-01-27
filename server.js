c852d2af8dfd7b86450eb0fc7409e635ebb60657
seby19 <sebyxavier19@gmail.com>
2019-01-27 18:11:51 +0530

    server js7

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express'),
-    path = require('path');
+
+const express = require('express');
+const path = require('path');
 const app =express()
-app.use(express.static('./dist/portfolio'));
+app.use(express.static(__dirname +'./dist/portfolio'));
 app.get('/*',(req , res) =>{
-    res.sendFile(path.join(__dirname,'/dist/portfolio/index.html'));
+    res.sendFile(path.join(__dirname +'/dist/portfolio/index.html'));
 });
 
 app.listen(process.env.PORT || 8080 , () =>
