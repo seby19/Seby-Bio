e8ff8848e218ed15e31becf3b163b664a5156dd5
seby19 <sebyxavier19@gmail.com>
2019-01-27 18:41:24 +0530

    server js8

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app =express()
-app.use(express.static(__dirname +'./dist/portfolio'));
+app.use(express.static(__dirname +'/dist/portfolio'));
 app.get('/*',(req , res) =>{
     res.sendFile(path.join(__dirname +'/dist/portfolio/index.html'));
 });
