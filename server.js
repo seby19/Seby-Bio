f435bca4a6efd6eba4564e3146b4ba0fdcba8fce
seby19 <sebyxavier19@gmail.com>
2019-01-27 17:35:38 +0530

    server js4

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'),
     path = require('path');
 const app =express()
-app.use(express.static('./src/index.html'));
+app.use(express.static('./src'));
 app.get('/*',(req , res) =>{
     res.sendFile(path.join(__dirname,'./src/index.html'));
 });
