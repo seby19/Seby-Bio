acb61b5268b98f0646c18d40d2816443bcefca14
seby19 <sebyxavier19@gmail.com>
2019-01-27 17:14:20 +0530

    Express

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express'),
+    path = require('path');
+const app =express()
+app.use(express.static('./dist/portfolio'));
+app.get('/*',(req , res) =>{
+    res.sendFile(path.join(__dirname,'./dist/portfolio/index.html'));
+});
+
+app.listen(process.env.PORT || 8080 , () =>
+{
+    console.log('server started');
+}
+)
